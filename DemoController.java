61cc16f494e75ddf5f5ea06b586760ab77ed9ab4
unknown <sixto.e.r@hotmail.com>
2020-04-11 22:49:03 -0500

    nuevo cambio

--- DemoController.java
+++ DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class DemoController {
 	
-	 @GetMapping("/greeting")
+	 @GetMapping("/greeting Nuevo cambio")
 	    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 	        model.addAttribute("name", name);
 	        return "greeting";
